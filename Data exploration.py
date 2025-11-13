e4459bec7f505a7626add76de9e05018145bf138
DrBidoof <alwyn.lynch23@gmail.com>
2025-11-13 13:21:24 -0500

    Update Data exploration.py

--- Data exploration.py
+++ Data exploration.py
@@ -7,5 +7,14 @@ Created on Thu Nov 13 12:07:00 2025
 
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()  
+
+homicide_path = os.getenv("HOMICIDE")
+
+theft5000_path = os.getenv("THEFT5000")
+
+## read
+df_homicide = pd.read_csv(homicide_path)
+df_theft5000 = pd.read_csv(theft5000_path)
